bb508253c55c2bf62fabd015ed37c0893871fb75
karanamsahithi <38149823+karanamsahithi@users.noreply.github.com>
2018-09-29 16:02:07 +0530

    Add files via upload

---
+++ r.js
@@ -0,0 +1,46 @@
+var rollnumber=localStorage.getItem("roll");
+
+
+
+if (!window.indexedDB) {
+
+console.log("indexed db is not working...!");
+}
+var request=window.indexedDB.open("sahiDB",1);
+request.onerror=e=>{
+  console.log(e);
+}
+request.onupgradeneeded=e=>{
+var dbname=e.target.result;
+dbname.createObjectStore("cse",{keyPath:"roll"});
+console.log("upgraded...!");
+}
+request.onsuccess=e=>{
+ var dbname=e.target.result;
+store=dbname.transaction("cse","readwrite").objectStore("cse");
+var data=store.get(rollnumber);
+data.onsuccess=e=>{
+  var res=e.target.result;
+  var main=getElementById('mainDiv');
+  var left=document.createElement("div");
+  left.classList.add("leftDiv");
+  var name=document.createElement("h3");
+  name.textContent=res.name;
+  left.appendChild(name);
+  main.appendChild(left);
+  var hr=document.createElement("hr");
+  ledt.appendChild(hr);
+  var roll=document.createElement("p");
+  roll.textContent=res.roll;
+  left.appendChild(roll);
+  var email=document.createElement("p");
+  email.textContent=res.email;
+  left.appendChild(email);
+  var right=document.createElement("div");
+  right.classList.add("rightDiv");
+  var co=document.createElement("p");
+  co.textContent=res.co;
+  main.appendChild(right);
+}
+ console.log("success...!");
+}
